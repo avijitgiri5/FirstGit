53a61878dafa46e60061502fd59a43186a996673
avijitgiri5 <iamavijitgiri@gmail.com>
2023-03-25 09:25:34 +0530

    dom start

---
+++ dom.js
@@ -0,0 +1,37 @@
+// //console.dir(document)
+// console.log(document.domain)    
+// //console.log(document.title) 
+document.title=123
+// console.log(document.head)
+// // console.log(document.all)
+// // console.log(document.all[10])
+// //document.all[10].textContent="Hello"
+// console.log(document.forms[0])
+// console.log(document.links)
+
+
+let headerTitle=document.getElementById('header-title');
+headerTitle.style.borderBottom="solid 3px #000"
+
+const additems=document.getElementById('title');
+additems.style.fontWeight='bold';
+additems.style.color='green'
+
+//let header=document.getElementById('main-header')
+// console.log(headerTitle);
+// headerTitle.textContent="Hello"
+// headerTitle.innerText="Goodby"
+//headerTitle.innerHTML='<h1>Hello</h1>'
+
+let items=document.getElementsByClassName('list-group-item')
+// console.log(items)
+items[0].textContent="Hello"
+items[0].style.fontWeight='bold'
+items[0].style.backgroundColor="yellow"
+for(let i=0;i<items.length;i++)
+[
+    items[i].style.backgroundColor='#f4f4f4',
+    items[i].style.fontWeight="bold",
+    items[i].style.color='red',
+    items[i].textContent="Hello"
+]
